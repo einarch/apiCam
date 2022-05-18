28a8a3fd7787a71d0900514c8844234afaf2147c
einar <unico.arkngl.97@gmail.com>
2022-05-17 21:34:47 -0400

    crearActividad

---
+++ crearActividad.php
@@ -0,0 +1,38 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Content-Type: text/html; charset=utf-8");
+$method = $_SERVER['REQUEST_METHOD'];
+	include 'bd/BD.php';
+    $mysqli = acceder();
+
+	$JSONData = file_get_contents("php://input");
+	$dataObject = json_decode($JSONData);       
+
+	    
+	$idU = $dataObject-> idUs;
+	$descripcion =	$dataObject-> descripcion;
+	
+	$fechahora = $dataObject-> fecha;
+	$idAct= '';	
+
+	$actividad = $dataObject-> actividad;
+	$ubicacion = $dataObject-> ubicacion;
+
+if ($mysqli->connect_error) {
+  die("Connection failed: " . $mysqli->connect_error);
+}
+$stmt = $mysqli->prepare("INSERT INTO actividades VALUES (?, ?, ?, ?, ?, ?)");
+$stmt->bind_param('iiss', $idAct, $idU, $actividad, $fechahora, $ubicacion, $descripcion);
+$stmt->execute();
+
+if ($stmt->affected_rows > 0) {
+	echo json_encode(array('Guardado'=>true, 'Mensaje' => 'La publicacion se guardo con exito.'));
+} else {
+	echo json_encode(array('Guardado'=>false, 'Mensaje' => 'La publicacion no se pudo guardar.')); 
+}
+$stmt->close();
+
+$mysqli->close();
+	
+?>

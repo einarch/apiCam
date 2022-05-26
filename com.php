534a37356d0124dbc1abbfa223390a7da8ed299a
einar <unico.arkngl.97@gmail.com>
2022-05-25 23:14:50 -0400

    obtener publicacion

--- com.php
+++ com.php
@@ -5,8 +5,10 @@ include 'bd/BD.php';
 header('Access-Control-Allow-Origin: *');
 
 if($_SERVER['REQUEST_METHOD']=='GET'){
-    if(isset($_GET['id'])){
-        $query="SELECT * FROM publicaciones WHERE id=".$_GET['id'];
+    if(isset($_GET['IDPUB'])){
+        $query="SELECT usuarios.NOMBRE, usuarios.APELLIDO, publicaciones.FECHAHORAP, publicaciones.DESCRIPCIONP, publicaciones.IMAGENP 
+        FROM usuarios 
+        INNER JOIN publicaciones ON usuarios.IDUSUARIO = publicaciones.IDUSUARIO WHERE IDPUB=".$_GET['IDPUB'];
         $resultado=metodoGet($query);
         echo json_encode($resultado->fetch(PDO::FETCH_ASSOC));
     }else{
---
+++ obtenPublicacion.php
@@ -0,0 +1,25 @@
+<?php
+
+include 'bd/BD.php';
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: access");
+
+
+if($_SERVER['REQUEST_METHOD']=='GET'){
+
+    $query="SELECT publicaciones.IDPUB, usuarios.NOMBRE, usuarios.APELLIDO, publicaciones.FECHAHORAP, publicaciones.DESCRIPCIONP, publicaciones.IMAGENP 
+    FROM usuarios 
+    INNER JOIN publicaciones ON usuarios.IDUSUARIO = publicaciones.IDUSUARIO WHERE IDPUB=".$_GET['IDPUB'];
+    $resultado=metodoGet($query);
+    echo json_encode($resultado->fetch(PDO::FETCH_ASSOC));
+
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
+
+header("HTTP/1.1 400 Bad Request");
+
+
+?>
